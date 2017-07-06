e6f2284fe4e3024af72a6fe3fb0fb4fcf0598ab2
Henrik Merlander <henrik.merlander@gmail.com>
2017-07-06 10:25:18 +0200

    Add ingredient provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { SearchProvider } from '../providers/search/search';
 import { DrinkProvider } from '../providers/drink/drink';
+import { IngredientProvider } from '../providers/ingredient/ingredient';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { DrinkProvider } from '../providers/drink/drink';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     SearchProvider,
-    DrinkProvider
+    DrinkProvider,
+    IngredientProvider
   ]
 })
 export class AppModule {}
---
+++ src/providers/ingredient/ingredient.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'RxJS';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class IngredientProvider {
+
+  constructor(public http: Http) { }
+
+  getIngredientByName(ingredientName: string): Observable<any> {
+    return this.http
+      .get('http://www.thecocktaildb.com/api/json/v1/1/search.php?i=' + ingredientName)
+      .map(res => res.json())
+  }
+}
