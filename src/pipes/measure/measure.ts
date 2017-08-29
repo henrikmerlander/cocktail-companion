e10dfc509e7bcab2f67e617df5192167b649af9a
Henrik Merlander <henrik.merlander@gmail.com>
2017-08-29 20:09:38 +0200

    Refactor measure pipe

--- src/pipes/measure/measure.ts
+++ src/pipes/measure/measure.ts
@@ -7,30 +7,26 @@ import * as convert from 'convert-units';
 export class MeasurePipe implements PipeTransform {
 
   transform(value: string, ...args) {
-    if (args[0] === 'imperial') {
+    if (args[0] === 'metric') {
       var split = value.toLowerCase().trim().split(' ');
       var measurement = split.pop();
-      if (measurement != 'ml') return value;
+
       try {
-        var fluidOunces = Math.ceil(convert(eval(split.join('+'))).from('ml').to('fl-oz') / 5) * 5
+        switch (measurement) {
+          case 'oz':
+            return this.ozToCl(split) + 'cl';
+          default:
+            return value;
+        }
       }
       catch (e) {
         return value;
       }
-      return fluidOunces + ' oz';
-    }
-    else if (args[0] === 'metric') {
-      var split = value.toLowerCase().trim().split(' ');
-      var measurement = split.pop();
-      if (measurement != 'oz') return value;
-      try {
-        var milliliter = Math.ceil(convert(eval(split.join('+'))).from('fl-oz').to('ml') / 5) * 5
-      }
-      catch (e) {
-        return value;
-      }
-      return milliliter + ' ml';
     }
     else return value;
+  }
+
+  ozToCl(split) {
+    return Math.ceil(convert(eval(split.join('+'))).from('fl-oz').to('cl'))
   }
 }
